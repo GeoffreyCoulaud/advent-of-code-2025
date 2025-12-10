6928d244e3789636a2fdcf4671cb57b407162e9a
Geoffrey Coulaud <geoffrey.coulaud@pictarine.com>
2025-12-10 22:10:31 +0100

    Day 8 (wip 3)

--- day-08/day-08.py
+++ day-08/day-08.py
@@ -1,8 +1,5 @@
 import logging
 from argparse import ArgumentParser, Namespace
-from collections import Counter
-from functools import reduce
-from operator import mul
 from typing import NamedTuple
 
 
@@ -17,47 +14,37 @@ class Vec3(NamedTuple):
     y: int
     z: int
 
-    def d2(self, other: "Vec3") -> float:
-        return sum((ca - cb) ** 2 for ca, cb in zip(self, other))
-
     def __str__(self):
         return ",".join(["%03d" % c for c in self])
 
 
-def find_circuit_index(junction_box_index: int, circuits: list[set[int]]) -> int:
-    for circuit_index, circuit in enumerate(circuits):
-        if junction_box_index in circuit:
-            return circuit_index
-    raise RuntimeError("Cannot happen")
+IndexedVec3 = tuple[int, Vec3]
+PairOfIndexedVec3 = tuple[IndexedVec3, IndexedVec3]
 
 
-def in_same_circuit(a: int, b: int, circuits: list[set[int]]) -> bool:
-    cai = find_circuit_index(a, circuits)
-    cbi = find_circuit_index(b, circuits)
-    return cai == cbi
+def pair_get_indexes(pair: PairOfIndexedVec3) -> tuple[int, int]:
+    (i, _), (j, _) = pair
+    return (i, j)
 
 
-def closest_junction_boxes(
-    positions: list[Vec3], circuits: list[set[int]]
-) -> tuple[int, int]:
-    assert len(circuits) > 1, "There must be at least 2 circuits"
+def pair_get_vectors(pair: PairOfIndexedVec3) -> tuple[Vec3, Vec3]:
+    (_, a), (_, b) = pair
+    return a, b
 
-    smallest_d2: float = float("+inf")
-    smallest_pair: tuple[int, int]
-    for i, first in enumerate(positions):
+
+def pair_distance_squared(pair: PairOfIndexedVec3) -> float:
+    return sum((ca - cb) ** 2 for ca, cb in zip(*pair_get_vectors(pair)))
+
+
+def build_sorted_pairs(positions: list[Vec3]) -> list[tuple[int, int]]:
+    """Create a list of unique pairs of indexes, sorted by smallest distance"""
+    pairs = list[PairOfIndexedVec3]()
+    for i in range(len(positions) - 1):
         for j in range(i + 1, len(positions)):
-            second = positions[j]
-            if in_same_circuit(i, j, circuits):
-                logging.debug("%s <---> %s : Skipped", str(first), str(second))
-                continue
-            d2 = first.d2(second)
-            if d2 < smallest_d2:
-                logging.debug(
-                    "%s <---> %s : New smallest %f", str(first), str(second), d2
-                )
-                smallest_d2 = d2
-                smallest_pair = (i, j)
-    return smallest_pair
+            pair = ((i, positions[i]), (j, positions[j]))
+            pairs.append(pair)
+    pairs.sort(key=pair_distance_squared)
+    return [pair_get_indexes(pair) for pair in pairs]
 
 
 def main() -> None:
@@ -68,51 +55,49 @@ def main() -> None:
     parser.add_argument("connections", type=int)
     parser.add_argument("count_to_multiply", type=int, default=3)
     args = parser.parse_args(namespace=CliArgs())
-
     with open(args.file_path, "r") as file:
         positions = [
             Vec3(*[int(coord) for coord in line.split(",")])
             for line in file.read().strip().splitlines()
         ]
-    circuits = [set([i]) for i in range(len(positions))]
 
-    # Connect the N closest pairs
-    for i in range(args.connections):
-        logging.debug("Iteration %02d", i)
+    # Start with each box in its own circuit
+    circuits = [{i} for i in range(len(positions))]
+    circuit_per_box = {i: i for i in range(len(positions))}
 
-        ai, bi = closest_junction_boxes(positions, circuits)
-        a = positions[ai]
-        b = positions[bi]
+    # Merge circuits with N connections
+    box_index_pairs: list[tuple[int, int]] = build_sorted_pairs(positions)
+    for i, (a_box_index, b_box_index) in enumerate(box_index_pairs[: args.connections]):
+        a_circuit_index = circuit_per_box[a_box_index]
+        b_circuit_index = circuit_per_box[b_box_index]
 
-        # Identify circuits
-        cai = find_circuit_index(ai, circuits)
-        ca = circuits[cai]
-        cbi = find_circuit_index(bi, circuits)
-        cb = circuits[cbi]
+        msg = "%d -  %s <-> %s"
+        msg_args = [i, positions[a_box_index], positions[b_box_index]]
 
-        # fmt: off
-        logging.info(
-            "Iteration %02d: Connecting "
-            + "%s %02d (circuit %02d size %02d) + "
-            + "%s %02d (circuit %02d size %02d)",
-            i, 
-            str(a), ai, cai, len(ca),
-            str(b), bi, cbi, len(cb)
-        )
-        # fmt: on
+        if a_circuit_index == b_circuit_index:
+            msg += " Skipped"
+            logging.info(msg, *msg_args)
+            continue
 
-        # Merge
-        circuits[cai] = ca | cb
-        circuits.pop(cbi)
+        # Assign box to new circuit
+        circuit_per_box[b_box_index] = a_circuit_index
+        circuits[b_circuit_index].remove(b_box_index)
+        circuits[a_circuit_index].add(b_box_index)
 
-        circuits_per_size = Counter(len(c) for c in circuits)
-        logging.debug("Circuit sizes : %s", str(circuits_per_size))
+        msg += " Merged into %d"
+        msg_args += [a_circuit_index]
+        logging.info(msg, *msg_args)
 
-    logging.info("Ended with %d circuits", len(circuits))
-    sorted_circuits = sorted(circuits, key=lambda c: len(c), reverse=True)
-    biggest = sorted_circuits[: args.count_to_multiply]
-    logging.info("Biggest: %s", ", ".join(str(b) for b in biggest))
-    result = reduce(mul, (len(c) for c in biggest))
+    # Sort the circuits by size
+    circuits = [c for c in circuits if len(c) > 0]
+    sorted_circuits = sorted(circuits, key=len, reverse=True)
+    for circuit in sorted_circuits:
+        logging.info(", ".join("%02d" % box_index for box_index in circuit))
+
+    # Compute the result
+    result = 1
+    for circuit in sorted_circuits[: args.count_to_multiply]:
+        result *= len(circuit)
     print(result)
 
 
