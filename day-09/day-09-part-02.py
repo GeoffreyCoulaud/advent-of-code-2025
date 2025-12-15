0bfce184bca5d564631b97beb7d75eeabc4ca5db
GeoffreyCoulaud <geoffrey.coulaud@gmail.com>
2025-12-15 01:18:12 +0100

    Wip 2 day 9 part 2

--- day-09/day-09-part-02.py
+++ day-09/day-09-part-02.py
@@ -1,6 +1,4 @@
 from argparse import ArgumentParser, Namespace
-from collections.abc import MutableSequence, Sequence
-from functools import lru_cache
 from typing import NamedTuple
 
 
@@ -15,43 +13,6 @@ def minmax(*args: int) -> tuple[int, int]:
 class Vec2(NamedTuple):
     x: int
     y: int
-
-
-@lru_cache(10)
-def count_crossings(pos: Vec2, outline: frozenset[Vec2]) -> int:
-    """
-    Raycast towards the left, from x,y counting the number of line intersections.
-    Starting on a line guarantees a crossing.
-    """
-    current = 1 if pos in outline else 0
-    if pos.x == 0:
-        return current
-    else:
-        return current + count_crossings(Vec2(pos.x - 1, pos.y), outline)
-
-
-WHITE = 0
-RED = 1
-GREEN = 2
-
-
-def get_slot(
-    x: int,
-    y: int,
-    data: Sequence[int],
-    stride: int,
-) -> int:
-    return data[x + y * stride]
-
-
-def put_slot(
-    value: int,
-    x: int,
-    y: int,
-    data: MutableSequence[int],
-    stride: int,
-) -> None:
-    data[x + y * stride] = value
 
 
 def main() -> None:
@@ -90,27 +51,11 @@ def main() -> None:
             min_y, max_y = minmax(prev.y, pos.y)
             for y in range(min_y + 1, max_y):
                 outline_positions.add(Vec2(pos.x, y))
-    frozen_outline_positions = frozenset(outline_positions)
-    del outline_positions
 
-    # Populate the inside
-    print("Populating inside")
-    inside_positions = set[Vec2]()
-    width = max_x + 1
-    height = max_y + 1
-    for y in range(height):
-        print("Line %03d of %dx%d grid" % (y, width, height))
-        for x in range(width):
-            # Ignore already colored slots
-            pos = Vec2(x, y)
-            if pos in frozen_outline_positions:
-                continue
-            # Count line crossings for coord.
-            # Even = outside, odd = inside
-            if count_crossings(pos, frozen_outline_positions) % 2 != 0:
-                inside_positions.add(pos)
-
-    # TODO - Parallelize this bad boy, lines are independent
+    # - Write the inside function using winding number algorithm
+    # - Find the first inside neighbor along the outline
+    # - Flood fill from that
+    # - Compress outline / inside regions into rectangles
 
     print("Done")
 
